dc7079890a231204817cc4676c5a408cfc178889
Stacey McCutchen <staceymccutchen@Isaiahs-MacBook-Air.local>
2023-07-19 20:55:15 -0400

    first commit

---
+++ src/com/promineotech/Application.java
@@ -0,0 +1,12 @@
+package com.promineotech;
+
+public class Application {
+
+	public static void main(String[]args) {System.out.println("My first Java Program!");
+	
+	}
+		// TODO Auto-generated method stub
+
+
+
+}
